706fae451893a54d144e55b01aaab13207888d5b
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-09-07 12:52:02 -0400

    more statistical analysis scripts.

--- 2013/fall/math130/Analysis/lib/statistics/summary.rb
+++ 2013/fall/math130/Analysis/lib/statistics/summary.rb
@@ -4,18 +4,29 @@ module Statistics
       @data = data.sort
     end
 
-    def summary(data = @data)
+    def five_number_summary(data = @data)
       return {
-        minimum: minimum(@data),
-        q1: q1(@data),
-        median: median(@data),
-        q3: median(@data),
-        maximum: maximum(@data)
+        minimum: minimum(data),
+        q1: q1(data),
+        median: median(data),
+        q3: q3(data),
+        maximum: maximum(data)
       }
     end
 
+    def extended_summary(data = @data)
+      summary = five_number_summary(data)
+      return summary.merge({
+        mean: mean(data),
+        iqr: iqr(data),
+        fences: fences(data),
+        outliers: outliers(data),
+        count: count(data)
+      })
+    end
+
     def minimum(data = @data)
-      @minimumdata[0]
+      data[0]
     end
 
     def q1(data = @data)
@@ -44,6 +55,59 @@ module Statistics
 
     def maximum(data = @data)
       data[data.length - 1]
+    end
+
+    def mean(data = @data)
+      return (sum(data) / count(data)).to_f
+    end
+
+    def iqr(data = @data)
+      q3(data) - q1(data)
+    end
+
+    def fences(data = @data)
+      lower = q1(data) + (1.5 * iqr(data))
+      upper = q3(data) + (1.5 * iqr(data))
+
+      return (lower..upper)
+    end
+
+    def outliers(data = @data)
+      outliers = []
+      range = fences(data)
+
+      data.each do |num|
+        unless range.include?(num)
+          outliers << num
+        end
+      end
+
+      return outliers
+    end
+
+    def sum(data = @data)
+      data.inject(0) do |accum, i|
+        accum + i
+      end
+    end
+
+    def count(data = @data)
+      return data.length
+    end
+
+    # s^2 = (sigma(y - ybar)^2) / (n - 1)
+    def variance(data = @data)
+      mean = mean(data)
+      sum = data.inject(0) do |accum, i|
+        accum +(i - mean)**2
+      end
+
+      return (sum / (count(data) - 1)).to_f
+    end
+
+    # sqrt(variance)
+    def standard_deviation(data = @data)
+      return Math.sqrt(self.variance)
     end
   end
 end
